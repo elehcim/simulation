52975235cfdccb6f30352f175ba74a650b54d48d
Michele Mastropietro <michele.mastropietro@ugent.be>
2017-10-06 14:34:17 +0200

    Use function and enable load of precomputed cog

--- plot_cog.py
+++ plot_cog.py
@@ -1,9 +1,9 @@
 # plot COG
 
 import argparse
-import enums
 import os
 import sys
+import collections
 import numpy as np
 import astropy.units as u
 import matplotlib.pyplot as plt
@@ -17,6 +17,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--sim")
 parser.add_argument('--dir', default='~/sim/nfw_c3/out')
 parser.add_argument('-n','--snap', default=None)
+parser.add_argument('--npy-file', help="Specify a file with cached COG data")
+
 args = parser.parse_args()
 
 snapshots = range(4,99)
@@ -24,50 +26,69 @@ snapshots = range(4,99)
 import chyplot
 getProp = chyplot.cglobals.plmap.getSecond
 
-cog = np.zeros((3, len(snapshots)), dtype=float)
-i = 0 
-for snap in snapshots:
-	dr = chyplot.CDataGadget(snap)
+
+def compute_cog(snapshots, directory, save_cache=True):
+	if not isinstance(snapshots, collections.Iterable):
+		snapshots = [snapshots]
+
+	cog = np.zeros((3, len(snapshots)), dtype=float)
 	fdir = os.path.expanduser(args.dir)
-	dr.setPrefix( fdir )
+	i = 0
+	for snap in snapshots:
+		dr = chyplot.CDataGadget(snap)
+		fdir = os.path.expanduser(args.dir)
+		dr.setPrefix( fdir )
 
-	# if args.snap is None:
-	# 	dr.set_file(dr.lastDump())
-	# else:
-	# 	dr.set_file(args.snap)
+		# if args.snap is None:
+		# 	dr.set_file(dr.lastDump())
+		# else:
+		# 	dr.set_file(args.snap)
 
-	print "reading file ", dr.filename()
-	data = dr.readFile()
-	print "snapshot time {} Gyr".format(data.time())
-	# data.rcom(True, enums.T_star, 0, 0, 0, True)
-	# data.vcom(True, enums.T_star)
-	# data.rotate(enums.T_gas, 2, True)
-	# data.convertUnits()
-	mass = np.array(data.getDataArray(enums.T_all, getProp('mass'), True))
-	# time.append(data.header) = np.array(data.getDataArray(enums.T_all, getProp('time'), True))
+		print "reading file ", dr.filename()
+		data = dr.readFile()
+		print "snapshot time {} Gyr".format(data.time())
+		# data.rcom(True, enums.T_star, 0, 0, 0, True)
+		# data.vcom(True, enums.T_star)
+		# data.rotate(enums.T_gas, 2, True)
+		# data.convertUnits()
+		mass = np.array(data.getDataArray(enums.T_all, getProp('mass'), True))
+		# time.append(data.header) = np.array(data.getDataArray(enums.T_all, getProp('time'), True))
 
-	x = np.array(data.getDataArray(enums.T_all, getProp('x'), True))
-	y = np.array(data.getDataArray(enums.T_all, getProp('y'), True))
-	z = np.array(data.getDataArray(enums.T_all, getProp('z'), True))
+		x = np.array(data.getDataArray(enums.T_all, getProp('x'), True))
+		y = np.array(data.getDataArray(enums.T_all, getProp('y'), True))
+		z = np.array(data.getDataArray(enums.T_all, getProp('z'), True))
 
-	tot_mass = mass.sum()
-	print "tot mass = {} 10^10 Msol".format(tot_mass)
+		tot_mass = mass.sum()
+		print "tot mass = {} 10^10 Msol".format(tot_mass)
 
-	cog[:,i] = ((x*mass).sum()/tot_mass,
-				(y*mass).sum()/tot_mass, 
-				(z*mass).sum()/tot_mass)
+		cog[:,i] = ((x*mass).sum()/tot_mass,
+					(y*mass).sum()/tot_mass, 
+					(z*mass).sum()/tot_mass)
 
-	i += 1
-	del dr  # closing file
+		i += 1
+		del dr  # closing file
+	if save_cache:
+		np.save("cog.npy", cog)
+	return cog
 
-np.save("cog.npy", cog)
+if __name__ == '__main__':
+	if not args.npy_file:
+		cog = compute_cog(snapshots, args.dir)
+	else:
+		cog = np.load(args.npy_file)
 
-fig = plt.figure()
-ax = fig.add_subplot(121, projection='3d')
-ax.scatter(*cog)
+	fig = plt.figure()
+	ax = fig.add_subplot(121, projection='3d')
+	ax.set_xlabel("x (kpc)")
+	ax.set_ylabel("y (kpc)")
+	ax.set_zlabel("z (kpc)")
+	ax.scatter(*cog)
 
-ax2 = fig.add_subplot(122)
-ax2.scatter(*cog[:2])
+	ax2 = fig.add_subplot(122)
+	ax2.set_xlabel("x (kpc)")
+	ax2.set_ylabel("y (kpc)")
+	ax2.scatter(*cog[:2])
 
-plt.tight_layout()
-plt.show()
+	plt.tight_layout()
+	plt.show()
+
