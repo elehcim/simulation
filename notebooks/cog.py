f7140fdc9a3fd172f343ec68bf854d8062021eec
Michele Mastropietro <michele.mastropietro@ugent.be>
2017-10-26 11:59:21 +0200

    Print advancement

--- notebooks/cog.py
+++ notebooks/cog.py
@@ -10,14 +10,14 @@ def compute_cog(snapshots, directory='', save_cache=True, cache_file="cog.npz"):
 
     cog = np.zeros((3, len(snapshots)), dtype=float)
     times = np.zeros(len(snapshots), dtype=float)
-    i = 0
-    for snap in tqdm.tqdm(snapshots):
+    for i, snap in enumerate(snapshots):
         # duck-typing in case the snapshots are strings
         if not isinstance(snap, pynbody.gadget.GadgetSnap):
             sim = pynbody.load(os.path.join(directory, snap))
         else:
             sim = snap
-#         print("snapshot time {} Gyr".format(sim.properties['time'].in_units('Gyr')))
+
+        print("{:03d} Analysing {} (time {:.2f} Gyr)".format(i, sim.filename, sim.properties['time'].in_units('Gyr')))
         times[i] = sim.properties['time'].in_units('Gyr')
         mass = sim['mass']
         pos = sim['pos']
@@ -26,9 +26,6 @@ def compute_cog(snapshots, directory='', save_cache=True, cache_file="cog.npz"):
         # print "tot mass = {} Msol".format(tot_mass)
         # cog[:,i] = np.array([(pos[:,0] * mass).sum(), (pos[:,1] * mass).sum(), (pos[:,2] * mass).sum()])/tot_mass
         cog[:,i] = np.sum(mass * pos.transpose(), axis=1) / tot_mass
-        
-        i += 1
-#     print(cog)
     if save_cache:
         np.savez(cache_file, times=times, cog=cog)
     return times, cog
