118af0bd908648a45734c16d91e60340e60d1eec
Michele Mastropietro <michele.mastropietro@ugent.be>
2017-10-30 14:07:46 +0100

    Refactor of snap_io

--- notebooks/snap_io.py
+++ notebooks/snap_io.py
@@ -2,10 +2,11 @@ import os
 import glob
 import pynbody
 
-moria_path = '/home/michele/sim/MoRIA/'
-kicked_path = '/home/michele/sim/MySimulations/Moria8Gyr_tidal'
+MORIA_PATH = '/home/michele/sim/MoRIA/'
+KICKED_PATH = '/home/michele/sim/MySimulations/Moria8Gyr_tidal'
 
 def snapshot_list(dirname, stem="snapshot_", fillwidth=4, include_dir=False):
+    """Return a list of the path to all the snapshots in the simulation folder"""
     if not os.path.isdir(dirname):
          raise IOError("{} is not a directory".format(dirname))
     if include_dir:
@@ -15,18 +16,38 @@ def snapshot_list(dirname, stem="snapshot_", fillwidth=4, include_dir=False):
     filelist.sort()
     return filelist
 
-def load_sim(path_to_snapshots):
-    snaplist = snapshot_list(path_to_snapshots, include_dir=True)
+def load_sim(sim_dir):
+    """Return a tuple of pynbody.SimSnap contained in the directory `sim_dir`"""
+    snaplist = snapshot_list(sim_dir, include_dir=True)
     simlist = tuple(pynbody.load(snap) for snap in snaplist)
     return simlist
 
-def load_moria_sim_and_kicked(sim_number, kicked=True, moria_path=moria_path, kicked_path=kicked_path):
-    snaps_path = os.path.join(moria_path, 'sim{}'.format(sim_number))
-    simlist = load_sim(snaps_path)
-    
-    if kicked:
-        ksnaps_path = os.path.join(kicked_path, 'sim{}'.format(sim_number), 'out')
-        ksimlist = load_sim(ksnaps_path)
-        return simlist, ksimlist
+def load_snap(sim_dir, snap_number):
+    """Return pynbody.SimSnap
+    if `snap_number is negative use it as a list index"""
+    snaplist = snapshot_list(sim_dir, include_dir=True)
+    if snap_number < 0:
+        return pynbody.load(snaplist[snap_number])
+    for snap in snaplist:
+        if "{:04d}".format(snap_number) in os.path.basename(snap):
+            break
     else:
-        return simlist
+        raise RuntimeError("No snap {} in simulation folder {}".format(snap_number, sim_dir))
+    return pynbody.load(snap)
+
+def load_moria(sim_number, snap_number=None, path=MORIA_PATH):
+    sim_dir = os.path.join(path, 'sim{}'.format(sim_number))
+    if snap_number is None:
+        return load_sim(sim_dir)
+    else:
+        return load_snap(sim_dir, snap_number)
+
+def load_kicked(sim_number, snap_number=None, path=KICKED_PATH):
+    sim_dir = os.path.join(path, 'sim{}'.format(sim_number), 'out')
+    if snap_number is None:
+        return load_sim(sim_dir)
+    else:
+        return load_snap(sim_dir, snap_number)
+
+def load_moria_sim_and_kicked(sim_number, moria_path=MORIA_PATH, kicked_path=KICKED_PATH):
+    return load_moria(sim_number, path=moria_path), load_kicked(sim_number, path=kicked_path)
