a8b92ccf2af52006c1b88dacccfdc0c9b4331215
Michele Mastropietro <michele.mastropietro@ugent.be>
2018-01-12 09:46:38 +0100

    use astropy table for reading sumfiles

--- analyze_sumfiles.py
+++ analyze_sumfiles.py
@@ -1,41 +1,38 @@
-import os
-import pandas as pd
-import shutil
-import matplotlib.pylab as plt
-import numpy as np
+import astropy.units as u
+from astropy.io import ascii
+from astropy.table import Table
 
-
-header = ["# nr","time","redshift","ngas","nstar","ndark","Mgas","Mstar","Mdark","MHI","r_e_L","r_e_M","r_e_M_DM",
+header = ["nr","time","redshift","ngas","nstar","ndark","Mgas","Mstar","Mdark","MHI","r_e_L","r_e_M","r_e_M_DM",
           "flatStar","flatDM","velDisp_star","velDisp_DM","kinE_star","kinE_DM","bound_star","bound_DM","SFR",
           "M_U","M_B","M_V","M_R","M_I","M_J","M_H","M_K","metall_star","FeH_star","MgFe_star","metall_lum","FeH_lum",
           "age_lum","Mgas_re","Mstar_re","Mdark_re","rcom_x","rcom_y","rcom_z","vcom_x","vcom_y","vcom_z",
           "L_x","L_y","L_z","I0","sig(I0)","r0","sig(r0)","n","sig(n)","mu0","sig(mu0)",
-          "gas<1Re","gas<2Re","gas<3Re","gas<4Re","gas<5Re","gas<10Re","gas<30Re","gas all",
-          "DM<1Re","DM<2Re","DM<3Re","DM<4Re","DM<5Re","DM<10Re","DM<30Re","DM all"]
+          "gas<1Re","gas<2Re","gas<3Re","gas<4Re","gas<5Re","gas<10Re","gas<30Re","gas_all",
+          "DM<1Re","DM<2Re","DM<3Re","DM<4Re","DM<5Re","DM<10Re","DM<30Re","DM_all"]
 
-def change_header(filename):
-    backup = filename + ".orig"
-    if os.path.isfile(backup):
-        raise IOError("{} already exists".format(backup))
-    shutil.copyfile(filename, backup)
-    with open(backup, "r") as b:
-        content = b.readlines()
-        with open(filename, "w") as f:
-            content[0] = content[0].replace("# nr", "   #").replace("gas all", "gas_all").replace("DM all", "DM_all")
-            # print(content[0])
-            for line in content:
-                f.write(line)
+units = {"time":u.kpc/u.km*u.s,"Mgas":10**6*u.Msun,"Mstar":10**6*u.Msun,"Mdark":10**6*u.Msun,
+          "MHI":10**6*u.Msun,"r_e_L":u.kpc,"r_e_M":u.kpc,"r_e_M_DM":u.kpc,
+          "velDisp_star":u.km/u.s,
+          "velDisp_DM":u.km/u.s,"kinE_star":u.km/u.s,"kinE_DM":10**6*u.Msun*u.km/u.s,
+          "SFR":u.Msun/u.yr,
+          "M_U":u.mag,"M_B":u.mag,"M_V":u.mag,"M_R":u.mag,"M_I":u.mag,"M_J":u.mag,"M_H":u.mag,"M_K":u.mag,
+          # "metall_star","FeH_star","MgFe_star","metall_lum","FeH_lum","age_lum",
+          "Mgas_re":10**6*u.Msun,"Mstar_re":10**6*u.Msun,"Mdark_re":10**6*u.Msun,
+          "rcom_x":u.km,"rcom_y":u.km,"rcom_z":u.km,"vcom_x":u.km/u.s,"vcom_y":u.km/u.s,"vcom_z":u.km/u.s,
+          "L_x":u.km**2/u.s,"L_y":u.km**2/u.s,"L_z":u.km**2/u.s}
+          # "r0":u.kpc,"sig(r0)":u.kpc,"mu0":u.mag * s.arcsec**2,"sig(mu0)":u.mag * s.arcsec**2}
 
+meta = {"metall_star":"Metallicity weighted with star mass",
+        "metall_lum":"Metallicity weighted with star luminosity (typically higher than metall_star)"}
 
-def get_sumfile(sumfilepath):
-    try:
-        change_header(sumfilepath)
-    except IOError:
-        print("Header already modified")
-    return pd.read_csv(sumfilepath, delim_whitespace=True, index_col=0)
+exclude_names = ["flatStar","flatDM","bound_star","bound_DM", "I0", 'sig(I0)']
 
+def get_sumfile(sumfilepath, **kwargs):
+    table = ascii.read(sumfilepath, format='commented_header', exclude_names=exclude_names, **kwargs)
+    for column, unit in units.items():
+        table[column].unit = unit
+    return table
 
-# def ()
 if __name__ == '__main__':
-    datpath = "/home/michele/sim/MySimulations/Moria8Gyr_tidal/sim60003/60003.dat"
-    df = get_sumfile(datpath)
+    datpath = "/home/michele/sim/MoRIA/results/sumfiles/69002.dat"
+    table = get_sumfile(datpath)
