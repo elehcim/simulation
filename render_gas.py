f5a2ff63518ce08c7039223a2a596f38b8f7cfc0
Michele Mastropietro <michele.mastropietro@ugent.be>
2018-01-15 14:34:43 +0100

    add velocity lateral and little aestetic fixes

--- render_gas.py
+++ render_gas.py
@@ -5,8 +5,8 @@ import matplotlib.pylab as plt
 
 from notebooks.snap_io import load_moria_sim_and_kicked, load_moria, load_kicked, load_sim
 
-# SIMNUMBER = "69002_p200.0_a600.0_r600.0_c8.15"
-SIMNUMBER = "69002_p598.0_a600.0_r600.0_c8.15"
+SIMNUMBER = "69002_p200.0_a600.0_r600.0_c8.15"
+# SIMNUMBER = "69002_p598.0_a600.0_r600.0_c8.15"
 kicked = True
 
 faceon = False
@@ -18,6 +18,17 @@ vmin=5e-4
 figsize = (15,15)
 
 plt.style.use("dark_background")
+
+
+import contextlib
+@contextlib.contextmanager
+def np_printoptions(*args, **kwargs):
+    original = np.get_printoptions()
+    np.set_printoptions(*args, **kwargs)
+    try:
+        yield
+    finally: 
+        np.set_printoptions(**original)
 
 def gas_image(sim, **kwargs):
     sim.properties.pop('boxsize', None)
@@ -31,37 +42,26 @@ def gas_image(sim, **kwargs):
         sim.g['smooth'] *= 2
     return img
 
-
-def main():
-    snap_list = load_kicked(SIMNUMBER) if kicked else load_moria(SIMNUMBER)
-    
-    folder = "pngs_{}".format(SIMNUMBER)
-    os.makedirs(folder, exist_ok=True )
-    
-    for sim in snap_list:
+def gas_velocity_lateral_view(sim, **kwargs):
+    sim.properties.pop('boxsize', None)
+    sim.g['smooth'] /= 2
+    pynbody.analysis.halo.center(sim, shrink_factor=0.9)
+    velocity = sim['vel'].mean(axis=0)
+    backup = sim['pos'].copy()
+    v_modulus = np.linalg.norm(velocity)
+    x_p = (velocity[1]  * sim['pos'][:,0] + velocity[0] * sim['pos'][:,1])/v_modulus 
+    y_p = (-velocity[0] * sim['pos'][:,0] + velocity[1] * sim['pos'][:,1])/v_modulus
+    sim['pos'][:,0] = x_p
+    sim['pos'][:,1] = y_p
+    try:
         snap = int(sim.filename[-4:])
-        fig, ax = plt.subplots(figsize=figsize)
-        if not faceon:
-            filename = os.path.join(folder,"gas_image_{:03d}.png".format(snap))
-            title = '$t={:5.2f}$ Gyr, snap={:03d}'.format(sim.properties['time'].in_units("Gyr"), snap)
-        else:
-            filename = os.path.join(folder,"gas_image_angmomfaceon_{:03d}.png".format(snap))
-            title = '$t={:5.2f}$ Gyr, snap={:03d} (angmomfaceon)'.format(sim.properties['time'].in_units("Gyr"), snap)
-        gas_image(sim, width=width, resolution=resolution, vmin=vmin, vmax=vmax, title=title, filename=filename);
-        print("Saved", filename)
-
-def save_orthogonal():
-    snap_list = load_kicked(SIMNUMBER) if kicked else load_moria(SIMNUMBER)
-    
-    folder = "pngs_{}".format(SIMNUMBER)
-    os.makedirs(folder, exist_ok=True )
-    
-    for sim in snap_list:
-        snap = int(sim.filename[-4:])
-        filename = os.path.join(folder,"gas_image_proj_{:03d}.png".format(snap))
-        fig = gas_orthogonal_projection(sim, width=width, resolution=resolution, vmin=vmin, vmax=vmax);
-        fig.savefig(filename)
-        print("Saved", filename)
+        with np_printoptions(precision=2):
+            title = '$t={:5.2f}$ Gyr, snap={}\nv = {} kpc/s'.format(sim.properties['time'].in_units("Gyr"), snap, velocity)
+        img = sph.image(sim.g, qty="rho", units="g cm^-2", title=title, **kwargs)
+    finally:
+        sim.g['smooth'] *= 2
+        sim['pos'] = backup
+    return img
 
 def gas_orthogonal_projection(sim, qty='rho', **kwargs):
     sim.properties.pop('boxsize', None)
@@ -96,8 +96,9 @@ def gas_orthogonal_projection(sim, qty='rho', **kwargs):
         ax_angmom.set_xlabel("$x/%s$" % u_st)
         ax_angmom.set_ylabel("$y/%s$" % u_st)
         ax_angmom.set_title("angular momentum faceon")
-
-        fig.subplots_adjust(top=0.9)
+        
+        fig.tight_layout()
+        fig.subplots_adjust(top=0.96, bottom=0.11)
         cbar_ax = fig.add_axes([0.2,  0.05, 0.6, 0.02])
         fig.colorbar(im, cax=cbar_ax, orientation='horizontal').set_label(qty+"/"+kwargs['units'])
         
@@ -109,6 +110,53 @@ def gas_orthogonal_projection(sim, qty='rho', **kwargs):
         sim['pos'] = backup
     return fig
 
+
+def main():
+    snap_list = load_kicked(SIMNUMBER) if kicked else load_moria(SIMNUMBER)
+    
+    folder = "pngs_{}".format(SIMNUMBER)
+    os.makedirs(folder, exist_ok=True )
+    
+    for sim in snap_list:
+        snap = int(sim.filename[-4:])
+        fig, ax = plt.subplots(figsize=figsize)
+        if not faceon:
+            filename = os.path.join(folder,"gas_image_{:03d}.png".format(snap))
+            title = '$t={:5.2f}$ Gyr, snap={:03d}'.format(sim.properties['time'].in_units("Gyr"), snap)
+        else:
+            filename = os.path.join(folder,"gas_image_angmomfaceon_{:03d}.png".format(snap))
+            title = '$t={:5.2f}$ Gyr, snap={:03d} (angmomfaceon)'.format(sim.properties['time'].in_units("Gyr"), snap)
+        gas_image(sim, width=width, resolution=resolution, vmin=vmin, vmax=vmax, title=title, filename=filename);
+        print("Saved", filename)
+
+def save_orthogonal():
+    snap_list = load_kicked(SIMNUMBER) if kicked else load_moria(SIMNUMBER)
+    
+    folder = "pngs_{}".format(SIMNUMBER)
+    os.makedirs(folder, exist_ok=True )
+    
+    for sim in snap_list:
+        snap = int(sim.filename[-4:])
+        filename = os.path.join(folder,"gas_image_proj_{:03d}.png".format(snap))
+        fig = gas_orthogonal_projection(sim, width=width, resolution=resolution, vmin=vmin, vmax=vmax);
+        fig.savefig(filename)
+        print("Saved", filename)
+
+def save_velocity_lateral_view():
+    snap_list = load_kicked(SIMNUMBER) if kicked else load_moria(SIMNUMBER)
+    
+    folder = "pngs_{}".format(SIMNUMBER)
+    os.makedirs(folder, exist_ok=True )
+    
+    for sim in snap_list:
+        snap = int(sim.filename[-4:])
+        fig, ax = plt.subplots(figsize=figsize)
+        filename = os.path.join(folder,"gas_image_vel_proj_{:03d}.png".format(snap))
+        title = '$t={:5.2f}$ Gyr, snap={:03d}'.format(sim.properties['time'].in_units("Gyr"), snap)
+        gas_image(sim, width=width, resolution=resolution, vmin=vmin, vmax=vmax, title=title, filename=filename);
+        print("Saved", filename)
+
 if __name__ == '__main__':
     # main()
-    save_orthogonal()
+    # save_orthogonal()
+    save_velocity_lateral_view()
