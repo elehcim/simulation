b15a4ef684f26a61c616a4cdb65e20bef106542f
Michele Mastropietro <michele.mastropietro@ugent.be>
2018-02-09 19:16:56 +0100

    add docstring to compute_cog

--- notebooks/cog.py
+++ notebooks/cog.py
@@ -4,7 +4,22 @@ import pynbody
 import collections
 import tqdm
 
-def compute_cog(snapshots, directory='', save_cache=True, cache_file="cog.npz", verbose=True, family=None):
+def compute_cog(snapshots, directory='', save_cache=False, cache_file="cog.npz", verbose=True, family=None):
+    """
+    Compute the center of gravity of a simulation
+
+    Args:
+        snapshots: Iterable of snapshots simulation. Can be either an pynbody.snapshot.SimSnap or a list of string (filenames).
+            In the latter case the `directory` argument is used as the containing folder.
+        save_cache: Save the resultsin a npz file
+        verbose: Print which file is currently read
+        family: The subfamily attribute to take into account, if `None` consider the whole snapshot.
+
+    Returns:
+        Tuple of numpy arrays (t, cog) where:
+            `t` is the array of the times of the snapshots in Gyr
+             `cog` is a 3 column array with the coordinates of the center of gravity positions for each snapshot
+    """
     if not isinstance(snapshots, collections.Iterable):
         snapshots = [snapshots]
 #     print("Processing {} files".format(len(snapshots)))
@@ -26,7 +41,7 @@ def compute_cog(snapshots, directory='', save_cache=True, cache_file="cog.npz", 
         times[i] = sim.properties['time'].in_units('Gyr')
         mass = sim['mass']
         pos = sim['pos']
-        
+
         tot_mass = mass.sum()
         # print "tot mass = {} Msol".format(tot_mass)
         # cog[:,i] = np.array([(pos[:,0] * mass).sum(), (pos[:,1] * mass).sum(), (pos[:,2] * mass).sum()])/tot_mass
