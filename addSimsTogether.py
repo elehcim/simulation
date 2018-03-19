5f0a48c9bcf4f54d80ac35236ff21b181d16fd5e
Michele Mastropietro <michele.mastropietro@ugent.be>
2018-03-19 17:02:51 +0100

    add script to join snaps together

--- addSimsTogether.py
+++ addSimsTogether.py
@@ -1,25 +1,25 @@
 import math
 import os, sys, time
-import getopt
 import numpy as np
+import argparse
 
-# different paths depending on if you run it on the cluster or on your computer
-sys.path.append("/home/rpverbek/programs/hyplot/hyplot_build/src/sip")
+parser = argparse.ArgumentParser()
 
-
-gadgetDir = "/home/rpverbek/programs/gadget2/"
-outputDir = "/home/rpverbek/programs/gadget2/output/"
+parser.add_argument('input_files', nargs='+', help='Input files')
+parser.add_argument('-o', '--output_file', '--out', help='Output file')
 
 import chyplot
 import enums
 
+args = parser.parse_args()
+
 # **** Parameters to be set by hand ***
 # simulations that have to be put together
-sims = [14001, 21001, 24001]
+# sims = [14001, 21001, 24001]
 # snapshots of galaxies that have to be merged
-snaps = [36, 36, 36]
+# snaps = [36, 36, 36]
 # name of the new simulation
-newsim = 10001
+# newsim = 10001
 # x, y, z => distance where gas cloud should be placed (in kpc)
 positions = [(0,0,0), (10, 10, 10), (10, -10, 10)]
 # vx, vy, vz => velocities of the gas cloud (in km/sec)
@@ -36,47 +36,56 @@ reader = chyplot.CDataGadget()
 writer = chyplot.CWriteGadget()
 
 
-for sim, snap, position, velocity in zip(sims, snaps, positions, velocities):
-    reader.setPrefix(os.path.join(outputDir, "sim%04.d"%sim))
-    reader.setRunNumber(sim)
-    reader.set_file(snap)
-
+for snap in args.input_files:
+    print snap
+    # reader.setPrefix(os.path.join(outputDir, "sim%04.d"%sim))
+    # reader.setRunNumber(sim)
+    # reader.set_file(snap)
+    reader.setFilename(snap)
     try:
         data = reader.readFile()
     except chyplot.IOError as e:
         print
         print "****Error reading file****"
-        print "simulation number:", sim
+        print snap
         print e
         print e.what()
         sys.exit(12)
 
-    data.rcom(True, enums.T_star)
-    data.vcom(True, enums.T_star)
+    # data.rcom(True, enums.T_star)
+    # data.vcom(True, enums.T_star)
 
     # set the distance of the gas cloud from the galaxy: x,y,z
-    x, y, z = position
-    data.translate(enums.T_all,x, y, z) 
-    # set the speed of the gas cloud: vx, vy, vz
-    vx, vy, vz = velocity
-    data.kick(enums.T_all, vx, vy, vz) 
+    # x, y, z = position
+    # data.translate(enums.T_all,x, y, z) 
+    # # set the speed of the gas cloud: vx, vy, vz
+    # vx, vy, vz = velocity
+    # data.kick(enums.T_all, vx, vy, vz)
 
-    print "used sim", sim, " snapshot ", snap, " which has time ", data.time()
+    print "used snapshot ", snap, " which has time ", data.time()
 
     if totalData == None:
         totalData = data
     else:
         # add datablocks together
-        totalData = totalData + data
+        try:
+            totalData = totalData + data
+        except chyplot.IOError as e:
+            print
+            print "****Error reading file****"
+            print snap
+            print e
+            print e.what()
+            sys.exit(12)
 
 
 try:
-    writer.writeFile(totalData, os.path.join(gadgetDir, "ICs", "sim%04.d"%newsim + ".gic"), enums.T_all)
-    print "Generated new IC file, sim", newsim, "with time", totalData.time()
+    writer.writeFile(totalData, args.output_file, enums.T_all)
+    print "Generated new IC file:", args.output_file, "with time", totalData.time()
 except chyplot.IOError as e:
     print
     print "****Error writing file****"
-    print "simulation number:", newsim
+    print args.output_file
     print e
     print e.what()
     sys.exit(13)
