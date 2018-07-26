5998066add41315ba663bfd912ecd651726268eb
Michele Mastropietro <michele.mastropietro@gmail.com>
2018-07-26 12:14:47 +0200

    Introduced SnapTime object, simplified code and added documentation

--- sim_duration.py
+++ sim_duration.py
@@ -9,28 +9,41 @@ from datetime import datetime
 def unix2time(t):
     return datetime.fromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S')
 
+
+class SnapTime(object):
+    """Contains time values of a GadgetSnap"""
+    def __init__(self, snap):
+        self._snap = snap
+        self.time = snap.header.time
+        self.gyr = snap.properties['time'].in_units('Gyr')
+        self.creation = os.path.getmtime(snap.filename)
+
+    def __str__(self):
+        return "Snap time: {:.4f} ({:.4f} Gyr)".format(self.time, self.gyr)
+
+    def __repr__(self):
+        return "Snap time: {:.4f} ({:.4f} Gyr). Created {}".format(self.time, self.gyr, unix2time(self.creation))
+
+
 class SimDuration(object):
-    """docstring for SimDuration"""
+    """Common utilities to compute duration of a Simulation"""
     def __init__(self, path):
         self.path = path
         f, l = load_first_last_snap(path)
         self.params = get_param_used(path)
         self.arrival = float(self.params['TimeMax'])
-        self.f, self.l = f, l
-        print('')
-        self.f_creation = os.path.getmtime(f.filename)
-        self.l_creation = os.path.getmtime(l.filename)
-        self.dt = self.l_creation - self.f_creation
+        self.f, self.l = SnapTime(f), SnapTime(l)
+        self.dt = self.l.creation - self.f.creation
         self.dt_day = self.dt/3600/24
-        self.gyr = l.properties['time'].in_units('Gyr') - f.properties['time'].in_units('Gyr')
+        self.gyr = self.l.gyr - self.f.gyr
         self._gyr_day = self.gyr/self.dt_day
-        self._simtime_day = (self.l.header.time-self.f.header.time)/self.dt_day
+        self._simtime_day = (self.l.time-self.f.time)/self.dt_day
 
     def __repr__(self):
-        s =  "First created: {}\n".format(unix2time(self.f_creation))
-        s += "Last created:  {}\n".format(unix2time(self.l_creation))
-        s += "First time:    {:.4f} ({:.4f} Gyr)\n".format(self.f.header.time, self.f.properties['time'].in_units('Gyr'))
-        s += "Last time:     {:.4f} ({:.4f} Gyr)\n".format(self.l.header.time, self.l.properties['time'].in_units('Gyr'))
+        s =  "First created: {}\n".format(unix2time(self.f.creation))
+        s += "Last created:  {}\n".format(unix2time(self.l.creation))
+        s += "First time:    {}\n".format(self.f)
+        s += "Last time:     {}\n".format(self.l)
         s += "Arrival:       {}\n".format(self.arrival)
         s += "Gyr/day:       {:.4f} ({:.4f} Gyr)\n".format(self._simtime_day, self.gyr_day)
         s += "ETA:           {}".format(self.eta())
@@ -47,13 +60,13 @@ class SimDuration(object):
     def eta(self, first=None, last=None):
         """Estimated time of arrival"""
         snaplist = snapshot_file_list(os.path.expanduser(self.path), include_dir=True)
-        first = self.f if first is None else snaplist[first]
-        last = self.l if last is None else snaplist[last]
+        first = self.f if first is None else SnapTime(snaplist[first])
+        last = self.l if last is None else SnapTime(snaplist[last])
 
         now = datetime.utcnow().timestamp()
-        
-        end = last.header.time
-        begin = first.header.time
+
+        end = last.time
+        begin = first.time
 
         eta = (self.arrival - begin) * self.dt / end
         return  unix2time(now + eta)
