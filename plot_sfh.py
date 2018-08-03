a9aff4af0512d2f18ba39a6e000e94dcec30b97d
Michele Mastropietro <michele.mastropietro@gmail.com>
2018-08-03 11:30:42 +0200

    squeeze bugs

--- plot_sfh.py
+++ plot_sfh.py
@@ -3,9 +3,13 @@ import pynbody
 import matplotlib.pylab as plt
 import numpy as np
 import argparse
+from pprint import pprint
 
 def plot_sfh(sim, plot_traj=False, figsize=(6,4), dpi=200):
+    pprint(sim.properties)
     fig, ax_sfh = plt.subplots(1, figsize=figsize, dpi=dpi)
+    fig.canvas.set_window_title(sim.sim_id)
+
     sim.plot_sfh(ax_sfh, label='SFR')
 
     if plot_traj:
@@ -15,16 +19,16 @@ def plot_sfh(sim, plot_traj=False, figsize=(6,4), dpi=200):
         ax_r1.set_ylabel("r [kpc]")
         lines2, labels2 = ax_r1.get_legend_handles_labels()
     else:
-        lines2, labels2 = []
+        lines2, labels2 = [], []
     # Manage the labels
     lines, labels = ax_sfh.get_legend_handles_labels()
     ax_sfh.legend(lines + lines2, labels + labels2, loc=0)
-    fig.show()
+    plt.show()
 
 def main(cli=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('sim', dest=sim_dir)
-    parsr.add_argument('-t', '--traj', action=store_true)
+    parser.add_argument('sim_dir')
+    parser.add_argument('-t', '--traj', action='store_true')
     args = parser.parse_args(cli)
 
     sim = simulation.Simulation(args.sim_dir)
