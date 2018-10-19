9d8b46b2c18abcff650acb46554f26ced395113c
Michele Mastropietro <michele.mastropietro@gmail.com>
2018-10-19 16:18:40 +0200

    use Sb inside color_plot

--- luminosity.py
+++ luminosity.py
@@ -175,62 +175,52 @@ def color_plot(snap, bands=('b','i'), width=10, resolution=500, mag_filter=29, g
 
     Returns
     -------
-    color_mag_arcsec2 : pynbody.SimArray
+    color : pynbody.SimArray
         The map of the color in mag/arcsec^2
-    Parameters
-    ----------
-
-    gaussian_sigma: in kpc is the sigma of the gaussian to convolve with the image, to make it more realistic
-
-    mag_filter: all region with magnitude/arcsec^2 higher than mag_filter will be set to NaN
     """
     # create color
     assert len(bands) == 2
 
+    if center:
+        pynbody.analysis.halo.center(snap.s, vel=False);
+
+    sb = list()
+    for band in bands:
+        sb.append(surface_brightness(snap.s, band=band, width=width, resolution=resolution, lum_pc2=False,
+                                     center=False, noplot=True, mag_filter=mag_filter))
+
+    # create color
+    color = sb[0] - sb[1]
+
+    if gaussian_sigma is not None:
+        sigma_pix = kpc2pix(gaussian_sigma, width, resolution)
+        print("Smoothing with gaussian kernel with sigma = {} pixel".format(sigma_pix))
+        color = gaussian_filter(color, sigma_pix)
+        
+        # from astropy.convolution import Gaussian2DKernel, convolve
+        # gaussian_2D_kernel = Gaussian2DKernel(sigma_pix)
+        # color = convolve(color, gaussian_2D_kernel)
+
+    if noplot:
+        return color
+
+    
     if subplot:
         fig, ax = subplot.figure, subplot
     else:
         fig, ax = plt.gcf(), plt.gca()
 
-    if center:
-        pynbody.analysis.halo.center(snap.s, vel=False);
-
-    # plot the two in 10^(-0.4) mag per pc**2
-    band0_pc2 = pynbody.plot.sph.image(snap.s, qty=bands[0] + '_lum_den', units='pc^-2', noplot=True, width=width, log=False, resolution=resolution, **kwargs)
-    band1_pc2 = pynbody.plot.sph.image(snap.s, qty=bands[1] + '_lum_den', units='pc^-2', noplot=True, width=width, log=False, resolution=resolution, **kwargs)
-
-    # convert to mag/arcsec**2
-    band0_mag_arcsec2 = convert_to_mag_arcsec2(band0_pc2)
-    band1_mag_arcsec2 = convert_to_mag_arcsec2(band1_pc2)
-
-    # Filter below a certain magnitude
-    if mag_filter is not None:
-        band0_mag_arcsec2[band0_mag_arcsec2 > mag_filter] = np.nan
-        band1_mag_arcsec2[band1_mag_arcsec2 > mag_filter] = np.nan
-
-    # create color
-    color_mag_arcsec2 = band0_mag_arcsec2 - band1_mag_arcsec2
-    color_name = '{}-{}'.format(*bands)
-
-    if gaussian_sigma is not None:
-        sigma_pix = kpc2pix(gaussian_sigma, width, resolution)
-        print("Smoothing with gaussian kernel with sigma = {} pixel".format(sigma_pix))
-        color_mag_arcsec2 = gaussian_filter(color_mag_arcsec2, sigma_pix)
-        
-        # from astropy.convolution import Gaussian2DKernel, convolve
-        # gaussian_2D_kernel = Gaussian2DKernel(sigma_pix)
-        # color_mag_arcsec2 = convolve(color_mag_arcsec2, gaussian_2D_kernel)
-
     cmap = plt.get_cmap(cmap_name)
     cmap.set_bad('black')
     extent = (-width/2, width/2, -width/2, width/2)
-    img = ax.imshow(color_mag_arcsec2, cmap=cmap, interpolation='none', extent=extent, origin='lower', vmin=vmin, vmax=vmax)
+    img = ax.imshow(color, cmap=cmap, interpolation='none', extent=extent, origin='lower', vmin=vmin, vmax=vmax)
     cbar = ax.figure.colorbar(img);
     ax.set_xlabel('x/kpc')
     ax.set_ylabel('y/kpc')
+    color_name = '{}-{}'.format(*bands)
     cbar.set_label('{} [mag/arcsec$^2$]'.format(color_name.upper()));
     # cont = ax.contour(img, cmap='flag', extent=(-width/2, width/2, -width/2, width/2))
     if title is not None:
         ax.set_title(title)
     plt.draw()
-    return color_mag_arcsec2
+    return color
