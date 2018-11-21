9c46ff2688c67a50ffabff46c781e64b5184510e
Michele Mastropietro <michele.mastropietro@gmail.com>
2018-11-21 16:16:11 +0100

    little fixes

--- simulation/particle_count.py
+++ simulation/particle_count.py
@@ -17,28 +17,45 @@ import asciiplotlib as apl
 #     header = _construct_gadget_header(header_block)
 #     print(header)
 
+
 class SimParticle(object):
     """Common utilities to compute duration of a Simulation"""
-    def __init__(self, path, first=0, last=-1):
+    def __init__(self, path, first=0, last=-1, width=50, height=15):
         self.path = path
-        sim = simulation.Simulation(os.path.expanduser(self.path), include_dir=True)
+        sim = simulation.Simulation(os.path.expanduser(self.path))
         self.sim = sim
             #         s += "Particles (start): dm:{} g:{} s:{}\n".format( len(self.first_snap.dm), len(self.first_snap.g), len(self.first_snap.s) )
             # s += "Particles (now):   dm:{} g:{} s:{}".format( len(self.last_snap.dm), len(self.last_snap.g), len(self.last_snap.s) )
+        self.width = width
+        self.height = height
+
         self.dm =  [len(s.dm) for s in sim]
         self.gas = [len(s.g) for s in sim]
         self.s = [len(s.s) for s in sim]
 
     def plot_graph(self):
-        plot_graph(self.sim.times, self.dm, self.gas, self.s)
+        plot_graph(self.sim.times, self.dm, self.gas, self.s, width=self.width, height=self.height)
+
+    def plot_multigraph(self):
+        plot_multigraph(self.sim.times, self.dm, self.gas, self.s, width=self.width, height=self.height)
 
 
-def plot_graph(times, dm , gas, stars):
+def plot_graph(times, dm , gas, stars, width=50, height=15):
     fig = apl.figure()
-    fig.plot(times, dm, label="dm", width=50, height=15)
-    fig.plot(times, gas, label="g", width=50, height=15)
-    fig.plot(times, stars, label="s", width=50, height=15)
+    fig.plot(times, dm, label="dm", width=width, height=height)
+    fig.plot(times, gas, label="g", width=width, height=height)
+    fig.plot(times, stars, label="s", width=width, height=height)
     fig.show()
+
+
+def plot_multigraph(times, dm , gas, stars, width=50, height=15):
+    fig = apl.SubplotGrid((1,3), padding=(0, 0))
+
+    fig[0,0].plot(times, dm, label="dm", width=width, height=height) # , symbol='points pt "o"'
+    fig[0,1].plot(times, gas, label="g", width=width, height=height) # , symbol='points pt "o"'
+    fig[0,2].plot(times, stars, label="s", width=width, height=height) # , symbol='points pt "o"'
+    fig.show()
+
 
 def main(cli=None):
     parser = argparse.ArgumentParser("Display some info on the simulation particles")
@@ -47,7 +64,9 @@ def main(cli=None):
     parser.add_argument(default=-1, type=int, nargs='?', help='Last file (ordinal, default=-1)', dest='last')
     args = parser.parse_args(cli)
     p = SimParticle(args.path, args.first, args.last)
-    p.plot_graph()
+    # p.plot_graph()
+    p.plot_multigraph()
+
 
 if __name__ == '__main__':
     main()
