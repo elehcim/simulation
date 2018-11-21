b936df9ecae56e364ffae9e4a5e3015bb6d56ba8
Michele Mastropietro <michele.mastropietro@gmail.com>
2018-11-21 13:27:07 +0100

    fix SFH in the box considering all the timesteps as denominator.

--- sfh_in_box.py
+++ sfh_in_box.py
@@ -1,5 +1,6 @@
 import simulation
 from itertools import tee
+import astropy.units as u
 
 
 def _pairwise(iterable):
@@ -9,37 +10,40 @@ def _pairwise(iterable):
     return zip(a, b)
 
 
-def sfh(simpath):
-    sim = simulation.Simulation(simpath)
+def sfh(sim_path):
+    sim = simulation.Simulation(sim_path)
 
     ns = list()
-    ds = list()
     ns_idx = list()
-    ds_idx = list()
     new_stars = list()
-    died_stars = list()
-    for (a0, a1) in _pairwise(sim):
+    snaps = sim
+    for (a0, a1) in _pairwise(snaps):
         s0, s1 = a0.s['iord'].view(np.ndarray), a1.s['iord'].view(np.ndarray)
         new_stars = np.setdiff1d(s1, s0)
-        died_stars = np.setdiff1d(s0, s1)
         ns_idx.append(np.where(np.isin(s1, new_stars))[0])
-        ds_idx.append(np.where(np.isin(s0, died_stars))[0])
         ns.append(new_stars)
-        ds.append(died_stars)
-
 
     # time contains left border of the time bin
-    time = list()
     mf = list()
-    for (idx, (a0, a1)) in zip(ns_idx, _pairwise(sim)):
-        time.append(a0.header.time)
-        mf.append(np.sum(a1.s['massform'][idx]).view(np.ndarray))
+    dts = list()
+    for (idx, (a0, a1)) in zip(ns_idx, _pairwise(snaps)):
+        mf.append(np.sum(a1.s['massform'][idx].in_units('Msol')).view(np.ndarray))
+        dts.append(a1.header.time - a0.header.time)
 
+    mf.append(0)
+    dts.append(np.inf)
 
-    times = np.array(time)
     massformed = np.array(mf)
+    dt = np.array(dts)
 
-    return times, massformed
+    t_conv_fac = (u.kpc/(u.km/u.s)).to(u.yr)
+
+    sfr = (massformed) / (dt * t_conv_fac)
+    # lets finish with a zero:
+    sfr = np.append(sfr, 0)
+    dt = np.append(dt, 0)
+
+    return dt, sfr
 
 if __name__ == '__main__':
 
@@ -76,6 +80,17 @@ if __name__ == '__main__':
     times = np.array(time)
     massformed = np.array(mf)
 
-    import astropy.units as u
-
     conv_fac = (u.kpc/(u.km/u.s)).to(u.Gyr)
+    dt, sfr = sfh(sim_path)
+
+    import matplotlib.pyplot as plt
+
+    fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(12,4))
+
+    pynbody.plot.stars.sfh(sim.snap_list[-1], subplot=ax1, bins=len(sim), trange=sim.t_range);
+    # ax1.grid()
+    ax2.plot(sim.times, sfr)
+    ax2.set_ylim(0, None)
+    # ax2.grid()
+    ax2.set_xlabel('time')
+    ax2.set_ylabel('SFR [Msol/yr]');
