b5b9402918dadfd250788f8bf526bf5f791b9556
Michele Mastropietro <michele.mastropietro@gmail.com>
2018-12-11 16:11:03 +0100

    fix parsers

--- simulation/parsers/parse_energy.py
+++ simulation/parsers/parse_energy.py
@@ -1,14 +1,14 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
 import pandas as pd
 
-header = ["time", "totIntEnergy", "totPotEnergy", "totKinEnergy", 
-"IntEnergy_gas", "PotEnergy_gas", "KinEnergy_gas", 
-"IntEnergy_dm", "PotEnergy_dm", "KinEnergy_dm", 
-"IntEnergy_disk", "PotEnergy_disk", "KinEnergy_disk", 
-"IntEnergy_bulge", "PotEnergy_bulge", "KinEnergy_bulge", 
-"IntEnergy_s", "PotEnergy_s", "KinEnergy_s", 
-"IntEnergy_bndry", "PotEnergy_bndry", "KinEnergy_bndry", 
+header = ["time", "totIntEnergy", "totPotEnergy", "totKinEnergy",
+"IntEnergy_gas", "PotEnergy_gas", "KinEnergy_gas",
+"IntEnergy_dm", "PotEnergy_dm", "KinEnergy_dm",
+"IntEnergy_disk", "PotEnergy_disk", "KinEnergy_disk",
+"IntEnergy_bulge", "PotEnergy_bulge", "KinEnergy_bulge",
+"IntEnergy_s", "PotEnergy_s", "KinEnergy_s",
+"IntEnergy_bndry", "PotEnergy_bndry", "KinEnergy_bndry",
 "mass_gas", "mass_dm", "mass_disk", "mass_bulge", "mass_s", "mass_bndry"]
 
 def parse_energy(fname="energy.txt"):
