fb749fde1e38db26e194c82fe294e589e9824593
Michele Mastropietro <michele.mastropietro@gmail.com>
2018-12-12 13:15:27 +0100

    add import of parsers

---
+++ simulation/parsers/__init__.py
@@ -0,0 +1,5 @@
+from .parse_cpu import parse_cpu
+from .parse_trace import parse_trace
+from .parse_timings import parse_timings
+from .parse_energy import parse_energy
+from .parse_info import parse_info
