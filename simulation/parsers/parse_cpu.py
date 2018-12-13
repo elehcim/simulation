76505d62c2690320e68743f758b15fcedc4bc794
Michele Mastropietro <michele.mastropietro@gmail.com>
2018-12-13 11:27:21 +0100

    update plot trace and typos

--- simulation/parsers/parse_cpu.py
+++ simulation/parsers/parse_cpu.py
@@ -3,17 +3,18 @@ import pandas as pd
 from itertools import islice
 
 
-# header from Gadget2/run.c:523
-header = ["tot", "grav", "hydro", "domain", 
-"potential", "drift", "kick", 
-"write_snap", "tree_walk", "tree_build", 
-"comm", "imbalance", "sph", 
-"comm_sph", "imb_sph", "neigh_adj", 
+# header from Gadget2/run.c:343
+header = ["tot", "grav", "hydro", "domain",
+"potential", "drift", "kick",
+"write_snap", "tree_walk", "tree_build",
+"comm", "imbalance", "sph",
+"comm_sph", "imb_sph", "neigh_adj",
 "PM", "peano", "add_phys", "star"]
 
 def parse_cpu(fname="cpu.txt"):
     with open(fname, "r") as f:
         nums = list()
+        # Skip odd lines, keep only even
         # usage of islice from here: https://stackoverflow.com/a/16022845/1611927
         for line in islice(f, 1, None, 2):
             nums.append([float(n) for n in line.split()])
