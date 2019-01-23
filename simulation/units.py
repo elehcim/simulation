e804393ab42f79e00d73e2ca88fa88d4708e9c82
Michele Mastropietro <michele.mastropietro@gmail.com>
2019-01-23 18:14:17 +0100

    update units

--- simulation/units.py
+++ simulation/units.py
@@ -1,8 +1,13 @@
 import pynbody
 
-gadget_time_units = pynbody.units.Unit('kpc km**-1 s')
+gadget_length_units = pynbody.units.Unit('kpc')
 gadget_vel_units = pynbody.units.Unit('km s**-1')
+gadget_mass_units = pynbody.units.Unit('1e10 Msol')
+# gadget_time_units = pynbody.units.Unit('kpc km**-1 s')
+gadget_time_units = gadget_length_units/gadget_vel_units
+
 gadget_acc_units = gadget_vel_units/gadget_time_units
+gadget_dens_units = gadget_mass_units/gadget_length_units**3
 
 
 amu = pynbody.units.NamedUnit("amu",1.660539e-27*pynbody.units.kg)
