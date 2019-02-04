d37502319f586495696cd84db901dab38378f3ad
Michele Mastropietro <michele.mastropietro@gmail.com>
2019-02-04 22:43:08 +0100

    use ax in plot_trace

--- simulation/plot/plot_trace.py
+++ simulation/plot/plot_trace.py
@@ -14,31 +14,33 @@ def load_trace(filename):
     return t
 
 
-def plot_trace(x, y, t=None):
+def plot_trace(x, y, t=None, ax=None):
     """
     Plot orbital path of the galaxy.
     Start is the green point, end is the red point.
     """
-    plt.plot(x, y)
-    plt.plot((0,0),'r+')
+    if ax is None:
+        fig, ax = plt.subplots()
+    ax.plot(x, y)
+    ax.plot((0,0),'r+')
     begin = x[0], y[0]
     end = x[-1], y[-1]
-    plt.plot(*begin,'go')
-    plt.plot(*end,'ro')
-    plt.xlabel('x [kpc]')
-    plt.ylabel('y [kpc]')
+    ax.plot(*begin,'go')
+    ax.plot(*end,'ro')
+    ax.set_xlabel('x [kpc]')
+    ax.set_ylabel('y [kpc]')
     if t is not None:
-        plt.title('t: {:5.2f} - {:5.2f} Gyr'.format(t[0], t[-1]))
-    plt.axes().set_aspect('equal')
-    plt.show()
+        ax.set_title('t: {:5.2f} - {:5.2f} Gyr'.format(t[0], t[-1]))
+    ax.set_aspect('equal')
+    # plt.show()
 
 
 def plot_trace_np(arr):
     plot_trace(arr[:, 1], arr[:, 2], arr[:, 0])
 
 
-def plot_trace_df(df):
-    plot_trace(df.x.values, df.y.values, df.t.values)
+def plot_trace_df(df, ax=None):
+    plot_trace(df.x.values, df.y.values, df.t.values, ax=ax)
 
 
 def _plot_trace_text_old(t):
@@ -72,6 +74,7 @@ def _plot_trace_df_old(df):
 def main(filename=sys.argv[-1]):
     arr = load_trace(filename)
     plot_trace_np(arr)
+    plt.show()
 
 
 if __name__ == '__main__':
