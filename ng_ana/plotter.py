e1875192be910e01bb06053bfeb48f5ecb09b10c
Michele Mastropietro <michele.mastropietro@gmail.com>
2019-02-05 16:15:34 +0100

    move axes adjustment to the end in order to use it on multiple sims.

--- ng_ana/plotter.py
+++ ng_ana/plotter.py
@@ -17,7 +17,7 @@ SIM_NUM = '71002'
 if len(sys.argv) == 2:
 	TRAJ = sys.argv[1]
 else:
-	TRAJ = 'p300_a800_r600'
+	TRAJ = 'p100_a800_r600'
 SIM = 'mb.{}'.format(SIM_NUM)
 MORIA_CACHE = 'moria{}.fits'.format(SIM_NUM)
 
@@ -48,7 +48,6 @@ ax_m_star.set_title('{} {}'.format(SIM, TRAJ));
 ax_r_eff.plot(df.t, df.r_eff)
 # ax_r_eff.set_xlabel("Time [Gyr]")
 ax_r_eff.set_ylabel("$r_{eff}$ [kpc]");
-ax_r_eff.set_ylim(0, 24)
 
 
 bins_sfh = 100
@@ -60,14 +59,12 @@ ax_sigma.plot(df.t, df.sigma_star, label='$\sigma_\star$')
 # ax_sigma.plot(df.t, df.sigma_gas, 'C0:', label='$\sigma_g$')
 # ax_sigma.set_xlabel("Time [Gyr]")
 ax_sigma.set_ylabel("$\sigma$ [km/s]");
-ax_sigma.set_ylim(0.0, 27)
 
 # [a.semilogy() for a in fig.axes]
 
 ram_pressure = pynbody.array.SimArray(sim.dens_trace.rho.values, gadget_dens_units).in_units('amu cm^-3')*sim.dens_trace.vel
 ax_ram.plot(sim.dens_trace.t*conv_fac, ram_pressure, color='g', label=r'$\rho v^2$')
 ax_ram.set_yscale('log')
-ax_ram.set_ylim(1e-4, 1.2)
 ax_ram.set_xlabel("Time [Gyr]")
 ax_ram.set_ylabel(r"""$\rho v^2$
 [amu cm$^{-3}$ (km/s)$^2$]""");
@@ -97,16 +94,19 @@ sfhist, thebins = pynbody.plot.sfh(moria_s, range=(df.t[0], df.t.iloc[-1]), subp
 if ax_sfh.get_ylim()[1] < 1.2 * max_sfr:
     ax_sfh.set_ylim(0.0, 1.2 * max_sfr)
 
-ax_sfh.set_ylim(0.0, 0.07)
 
-# ax_m_star.set_ylim(bottom=0, top=3e8)
-# ax_r_eff.set_ylim(bottom=0, top=4.5)
-# ax_sigma.set_ylim(bottom=0, top=39)
-# if TRAJ[1:].startswith() == '100':
-#     ax_ram.set_ylim(bottom=0, top=2)
-# elif TRAJ[1] == '3':
-#     ax_ram.set_ylim(bottom=0, top=1.69)
-
+if SIM_NUM.startswith('71'):
+    ax_r_eff.set_ylim(0, 24)
+    ax_sfh.set_ylim(0.0, 0.07)
+    ax_sigma.set_ylim(0.0, 27)
+    ax_ram.set_ylim(1e-4, 1.2)
+elif SIM_NUM.startswith('69'):
+    ax_r_eff.set_ylim(0, 30)
+    ax_sfh.set_ylim(0.0, 0.12)
+    ax_sigma.set_ylim(0.0, 35)
+    ax_ram.set_ylim(1e-4, 1.2)
+elif SIM_NUM.startswith('62'):
+    pass
 
 fig.subplots_adjust(hspace=0)
 
