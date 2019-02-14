9b0ff4260334501ceeb5846d30df6ed89e56fce0
Michele Mastropietro <michele.mastropietro@gmail.com>
2019-02-14 20:48:07 +0100

    Fix star metals and fix the radius value to 10

--- ng_ana/data_pd/dump_features.py
+++ ng_ana/data_pd/dump_features.py
@@ -10,13 +10,15 @@ import glob
 from astropy.table import Table
 import astropy.units as u
 
-gCosmoOrb_rdwarf = 50
+# gCosmoOrb_rdwarf = 50  # kpc
+# R_EFF_BORDER = gCosmoOrb_rdwarf
+R_EFF_BORDER = 10  # Same as hyplot
 
 def sigma(vel):
     return np.sqrt(((vel - vel.mean(axis=0))**2).mean())
 
 
-def dump_features(sim, output_name, radius=5, do_sfr=True):
+def dump_features(sim, output_name, radius=R_EFF_BORDER, do_sfr=True):
     """
     Write the result of the standard analysis on a pickle file.
 
@@ -27,18 +29,20 @@ def dump_features(sim, output_name, radius=5, do_sfr=True):
     sphere = pynbody.filt.Sphere(radius * pynbody.units.kpc)
     mass_star = list()
     r_eff = list()
+    r_eff3d = list()
     sigma_star = list()
     sigma_gas = list()
     metals_star = list()
 
     for snap in tqdm.tqdm(sim.snap_list):
         pynbody.analysis.halo.center(snap.s, vel=False)
-        mass_star.append(snap.s[sphere]['mass'].sum().in_units('Msol'))
-        sphere_r_eff = pynbody.filt.Sphere(gCosmoOrb_rdwarf * pynbody.units.kpc)
-        r_eff.append(pynbody.analysis.luminosity.half_light_r(snap.s[sphere_r_eff]))
+        star_m = snap.s[sphere]['mass'].sum().in_units('Msol')
+        mass_star.append(star_m)
+        r_eff.append(pynbody.analysis.luminosity.half_light_r(snap.s[sphere],  cylindrical=True))
+        r_eff3d.append(pynbody.analysis.luminosity.half_light_r(snap.s[sphere], cylindrical=False))
         sigma_star.append(sigma(snap.s[sphere]['vel']))
         sigma_gas.append(sigma(snap.g[sphere]['vel']))
-        metals_star.append(snap.s[sphere]['metals'].sum())
+        metals_star.append((snap.s[sphere]['metals'] * snap.s[sphere]['mass']).sum()/star_m)
         #TODO
 
     # DUMP
@@ -54,40 +58,40 @@ def dump_features(sim, output_name, radius=5, do_sfr=True):
                  'sigma_star':np.array(sigma_star) * u.km/u.s,
                  'sigma_gas':np.array(sigma_gas) * u.km/u.s,
                  'r_eff':np.array(r_eff) * u.kpc,
+                 'r_eff3d':np.array(r_eff3d) * u.kpc,
                  'sfr':np.array(sfr) * u.solMass/u.yr,
                  'metals_star':np.array(metals_star),
                  },
                  )
     tbl.write(output_name, format='fits', overwrite=True)
 
-def make_df(pickle_file):
-    # Gyr, Msol,   km/s    ,  km/s    , kpc  ,Msol/yr
-    times, mass_star, sigma_star, sigma_gas, r_eff,   sfr    = pickle.load(open(pickle_file, 'rb'))
-    df = pd.DataFrame({'t': times,
-                       'mass_star':mass_star,
-                       'sigma_star':sigma_star,
-                       'sigma_gas':sigma_gas,
-                       'r_eff':r_eff,
-                       'sfr':sfr})
-    return df
+# def make_df(pickle_file):
+#     # Gyr, Msol,   km/s    ,  km/s    , kpc  ,Msol/yr
+#     times, mass_star, sigma_star, sigma_gas, r_eff,   sfr    = pickle.load(open(pickle_file, 'rb'))
+#     df = pd.DataFrame({'t': times,
+#                        'mass_star':mass_star,
+#                        'sigma_star':sigma_star,
+#                        'sigma_gas':sigma_gas,
+#                        'r_eff':r_eff,
+#                        'sfr':sfr})
+#     return df
 
-def make_astropy_table(pickle_file):
-    times, mass_star, sigma_star, sigma_gas, r_eff = pickle.load(open(pickle_file, 'rb'))
-    tbl = Table({'t': times * u.Gyr,
-                 'mass_star':mass_star * u.solMass,
-                 'sigma_star':sigma_star * u.km/u.s,
-                 'sigma_gas':sigma_gas * u.km/u.s,
-                 'r_eff':r_eff * u.kpc,
-                 # 'sfr':sfr * u.solMass/u.yr,
-                 })
-    return tbl
+# def make_astropy_table(pickle_file):
+#     times, mass_star, sigma_star, sigma_gas, r_eff = pickle.load(open(pickle_file, 'rb'))
+#     tbl = Table({'t': times * u.Gyr,
+#                  'mass_star':mass_star * u.solMass,
+#                  'sigma_star':sigma_star * u.km/u.s,
+#                  'sigma_gas':sigma_gas * u.km/u.s,
+#                  'r_eff':r_eff * u.kpc,
+#                  # 'sfr':sfr * u.solMass/u.yr,
+#                  })
+#     return tbl
 
 if __name__=='__main__':
     moria_list = [62002, 71002, 69002]
-    radius = 5
     for moria_name in moria_list:
         moria = simulation.MoriaSim(str(moria_name))
-        dump_features(moria, "moria{}.fits".format(moria_name), radius=radius, do_sfr=False)
+        dump_features(moria, "moria{}.fits".format(moria_name), do_sfr=False)
     # sim_list = ['mb.62002_p300_a800_r600']
     # SIM_PATH ='/media/michele/My Book/Michele/MySimulations/MovingBox/np'
     # for sim_name in sim_list:
