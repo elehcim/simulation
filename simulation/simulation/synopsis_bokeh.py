6fdc14e94e709d2dc07059c82c6db7f718eed2b2
Michele Mastropietro <michele.mastropietro@gmail.com>
2019-03-19 20:13:35 +0100

    many improvements

--- simulation/simulation/synopsis_bokeh.py
+++ simulation/simulation/synopsis_bokeh.py
@@ -16,13 +16,12 @@ from bokeh.resources import INLINE
 # output_notebook(resources=INLINE)
 
 
-def get_maps_img():
-    maps = sorted(glob.glob(os.path.join(SSAM_DIR, 'maps_img', 'maps_*.png')))
+def get_maps_img(img_dir):
+    maps = sorted(glob.glob(os.path.join(img_dir, 'maps_img', 'maps_*.png')))
     d = dict()
     for m in maps:
         d[int(m[-7:-4])-1] = m
     print(len(maps))
-    print(maps[0:5])
     return d
 
 
@@ -48,15 +47,10 @@ ssam_df = ssam_tbl.to_pandas()
 df = Table.read(TBL).to_pandas()
 
 # Add maps:
-# maps = sorted(glob.glob(os.path.join(SSAM_DIR, 'maps_img', 'maps_*.png')))
-# print(len(maps))
-# print(maps[0:5])
-# df.dropna()
-# df['maps'] = maps
-d = get_maps_img()
+d = get_maps_img(SSAM_DIR)
 df['maps'] = pd.Series(d)
 # df['maps'] = df['maps'].fillna('')
-df.sfr = np.ones(len(df))
+# df.sfr = np.ones(len(df))
 
 # Merge data
 for col in ('time', 'r_eff_kpc', 'r_eff_kpc3d', 'lambda_r', 'ellip', 'theta', 'n', 'Lx', 'Ly', 'Lz', 'mag_v'):  # TODO compare these two 'r_eff_kpc', 'r_eff_kpc3d'
@@ -66,7 +60,7 @@ for col in ('time', 'r_eff_kpc', 'r_eff_kpc3d', 'lambda_r', 'ellip', 'theta', 'n
 # Do averages
 # df['lambda_r_mean'] = df.lambda_r.rolling(window=WINDOW).agg(np.mean)
 # df['ellip_mean'] = df.ellip.rolling(window=WINDOW).agg(np.mean)
-for col in ('lambda_r', 'ellip'):
+for col in ('lambda_r', 'ellip', 'n'):
     df[col+'_mean'] = df[col].rolling(window=WINDOW, win_type='gaussian', center=True).mean(std=5)
 
 
@@ -86,8 +80,10 @@ p.xgrid.grid_line_color = None
 p.ygrid.grid_line_color = None
 p.outline_line_alpha = 0
 
-#####
 COMMON_FIG_ARGS = dict(plot_width=450, plot_height=350, tools=TOOLS)
+
+
+##### MAPS
 
 s1 = figure(**COMMON_FIG_ARGS)
 # s1.line(source.data['time'], source.data['lambda_r_mean'], color="navy")
@@ -106,7 +102,9 @@ circle = s1.circle(x=source.data['time'][WINDOW], y=source.data['lambda_r_mean']
 s1.title.text = "Specific Stellar Angular Momentum"
 s1.xaxis.axis_label = 'time'
 s1.yaxis.axis_label = 'lambda_mean ' + str(WINDOW)
-############
+
+
+#### REFF
 
 s2 = figure(**COMMON_FIG_ARGS, x_range=s1.x_range)
 r2 = s2.multi_line(xs=[source.data['time']]*2, ys=[source.data['r_eff_kpc'], source.data['r_eff_kpc3d']],
@@ -116,16 +114,19 @@ legend2 = Legend(items=[
     LegendItem(label="r_eff", renderers=[r2], index=0),
     LegendItem(label="3D r_eff", renderers=[r2], index=1),
 ])
+legend2.location = "top_left"
+s2.add_layout(legend2)
+
 s2.y_range.start = 0
 s2.title.text = "Effective radius"
 s2.xaxis.axis_label = 'time'
 s2.yaxis.axis_label = 'r_eff'
-legend2.location = "top_left"
-s2.add_layout(legend2)
 
 vline = Span(dimension='height', line_color='red')
 s2.add_layout(vline)
-##########
+
+
+#### ORBIT
 
 s3 = figure(**COMMON_FIG_ARGS, match_aspect=True)
 
@@ -135,7 +136,9 @@ s3.cross(x=0, y=0, color='green', size=5)
 s3.title.text = "Orbit"
 s3.xaxis.axis_label = 'x [kpc]'
 s3.yaxis.axis_label = 'y [kpc]'
-######################
+
+
+#### MAG
 
 s4 = figure(**COMMON_FIG_ARGS, x_range=s1.x_range)
 
@@ -145,24 +148,34 @@ s4.y_range.flipped = True
 s4.title.text = "Magnitude"
 s4.xaxis.axis_label = 'time'
 s4.yaxis.axis_label = 'Magnitude (V band)'
-######################
+
+
+#### ELLIP
 
 # tab1 = Panel(child=p1, title="xy")
 # tab2 = Panel(child=p2, title="size")
 
 # tabs = Tabs(tabs=[ tab1, tab2 ])
 
-
 s5 = figure(**COMMON_FIG_ARGS, x_range=s1.x_range)
 
-s5.line(source.data['time'], source.data['ellip_mean'])
+l5a = s5.line(source.data['time'], source.data['ellip_mean'], color='navy')
+l5b = s5.line(source.data['time'], source.data['ellip'], line_alpha=0.4, color='green')
 circle_ell_t = s5.circle(x=source.data['time'][WINDOW], y=source.data['ellip_mean'][WINDOW], color='red')
 s5.y_range.start = 0
 s5.title.text = "Ellipticity"
 s5.xaxis.axis_label = 'time'
 s5.yaxis.axis_label = 'ellip_mean ' + str(WINDOW)
+legend5 = Legend(items=[
+    LegendItem(label='ellip_mean ' + str(WINDOW), renderers=[l5a], index=0),
+    LegendItem(label="ellip", renderers=[l5b], index=1),
+])
+legend5.location = "top_left"
+legend5.border_line_alpha=0.2
+s5.add_layout(legend5)
 
-###########
+
+#### ELLIP-LAMBDA
 s6 = figure(**COMMON_FIG_ARGS)
 
 y = df.time.values
@@ -176,8 +189,11 @@ circle_ell_l = s6.circle(x=source.data['ellip_mean'][WINDOW], y=source.data['lam
                          size=el_c.glyph.size+2, fill_color=None, line_color='red')
 color_bar = ColorBar(color_mapper=mapper['transform'], width=12, location=(0, 0))
 s6.add_layout(color_bar, 'right')
+s6.xaxis.axis_label = 'ellip_mean ' + str(WINDOW)
+s6.yaxis.axis_label = 'lambda_mean ' + str(WINDOW)
 
-######################
+
+#### SFR
 
 s7 = figure(**COMMON_FIG_ARGS, x_range=s1.x_range)
 
@@ -186,22 +202,34 @@ circle_sfh = s7.circle(x=source.data['time'][0], y=source.data['sfr'][0], color=
 s7.title.text = "SFH"
 s7.xaxis.axis_label = 'time'
 s7.yaxis.axis_label = 'SFR [Msol/yr]'
-######################
+
+
+##### SERSIC
 
 s8 = figure(**COMMON_FIG_ARGS, x_range=s1.x_range)
 
-s8.line(source.data['time'], source.data['n'])
-circle_n = s8.circle(x=source.data['time'][0], y=source.data['n'][0], color='red')
+l8a = s8.line(source.data['time'], source.data['n_mean'], color='navy')
+l8b = s8.line(source.data['time'], source.data['n'], line_alpha=0.4, color='green')
+circle_n = s8.circle(x=source.data['time'][WINDOW], y=source.data['n_mean'][WINDOW], color='red')
 s8.y_range.start = 0
 s8.title.text = "Sersic Index"
 s8.xaxis.axis_label = 'time'
-s8.yaxis.axis_label = 'n'
+s8.yaxis.axis_label = 'n_mean ' + str(WINDOW)
+legend8 = Legend(items=[
+    LegendItem(label='n_mean ' + str(WINDOW), renderers=[l8a], index=0),
+    LegendItem(label="n", renderers=[l8b], index=1),
+])
+legend8.location = "top_left"
+legend8.border_line_alpha=0.2
+s8.add_layout(legend8)
 
-######################
+
+
+#### GENERAL
 for _plot in [s1, s2, s3, s4, s5, s6, s7, s8]:
     _plot.toolbar.autohide = True
 
-###
+#### CALLBACK
 
 cb = CustomJS(args=dict(span=vline, im=im, source=source, circle=circle.glyph,
                         cross=cross.glyph, circle_mag=circle_mag.glyph,
@@ -220,7 +248,7 @@ cb = CustomJS(args=dict(span=vline, im=im, source=source, circle=circle.glyph,
     csfr.y=source.data['sfr'][cb_obj.value];
 
     cn.x=source.data['time'][cb_obj.value];
-    cn.y=source.data['n'][cb_obj.value];
+    cn.y=source.data['n_mean'][cb_obj.value];
 
     cross.x=source.data['x'][cb_obj.value];
     cross.y=source.data['y'][cb_obj.value];
@@ -233,6 +261,5 @@ cb = CustomJS(args=dict(span=vline, im=im, source=source, circle=circle.glyph,
 slider = Slider(start=0, end=len(df)-1, value=0, step=1, title="image number", callback=cb)
 
 l = layout([p, slider, row(column(s1, s2), column(s3, s4), column(s5, s6), column(s7, s8))])
-# l = layout([p, slider, row(column(s1, s2))])
 
 save(l)
