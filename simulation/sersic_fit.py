ddc296c9f5c46f3d6582e60935fe44922d400b4c
Michele Mastropietro <michele.mastropietro@gmail.com>
2019-03-21 21:39:38 +0100

    new fitting procedure

--- simulation/sersic_fit.py
+++ simulation/sersic_fit.py
@@ -8,33 +8,41 @@ import numpy as np
 def order_of_magnitude(x):
     return np.floor(np.log10(x))
 
-def sersic_fit(profile, r_eff, n_0, deviation_param=0.1, verblevel=0):
-    r = profile['rbins']
-    sb = profile['sb']
+def compute_rms_error(x, y):
+    return np.sqrt(np.sum((x-y)**2)/len(x))
+
+def sersic_fit(sb_profile, r_eff, n_0, deviation_param=0.2, verblevel=0):
+    r = sb_profile['rbins']
+    sb = sb_profile['sb']
     idx = np.digitize(r_eff, r) - 1
     ampl = 10**(-0.4*sb)[idx]
     oom = 10**(order_of_magnitude(ampl))
     # print(oom)
-    sersic_init = models.Sersic1D(amplitude=ampl/oom, r_eff=r_eff, n=n_0,
-                                  fixed={'amplitude': False,
-                                         'r_eff': False,
-                                         'n': False,
-                                         },
-                                  # Allow wobbling a bit
-                                  bounds={'r_eff':(r_eff*(1-deviation_param), r_eff*(1+deviation_param)),
-                                          #'amplitude':(ampl/oom*(1-deviation_param), ampl/oom*(1+deviation_param)),
-                                         },
-                                 )
+    sersic_init = Sersic1D(amplitude=ampl/oom, r_eff=r_eff, n=n_0,
+                           fixed={'amplitude': False,
+                                  'r_eff': False,
+                                  'n': False,
+                                  },
+                           # Allow wobbling a bit
+                           bounds={'r_eff':(r_eff*(1-deviation_param), r_eff*(1+deviation_param)),
+                                   #'amplitude':(ampl/oom*(1-deviation_param), ampl/oom*(1+deviation_param)),
+                                   'n':(max(n_0-5, 0), n_0+5),  # That's the trick. I'm assuming something more or less continuos
+                                  },
+                          )
 
     # sersic_init.amplitude.min = 1e-11
-    sersic_init.n.min = 0.1
-    sersic_init.n.max = 10
+    # sersic_init.n.min = 0.1
+    # sersic_init.n.max = 10
     # print("Bounds: ", sersic_init.bounds)
-    fitter = fitting.SLSQPLSQFitter()
-    s_fit = fitter(sersic_init, r, 10**(-0.4*sb)/oom, verblevel=verblevel)#, acc=1e-10)
-    s_fit.amplitude *= oom
-    # print(s_fit)
-    return s_fit
+    # fitter = fitting.LevMarLSQFitter()
+    # fitter = fitting.SLSQPLSQFitter() # <--- this causes some error
+    # The SimplexLSQFitter seems the best.
+    fitter = fitting.SimplexLSQFitter()
+    fitted_model = fitter(sersic_init, r, 10**(-0.4*sb)/oom, maxiter=1000)#, verblevel=verblevel)#, acc=1e-10)
+    fitted_model.amplitude *= oom
+    rms_error = compute_rms_error(sb, -2.5*np.log10(fitted_model(r)))
+    # print(fitted_model)
+    return fitted_model, fitter, rms_error
 
 def plot_fitted_profile(p, fit):
     plt.figure(figsize=(8,5))
