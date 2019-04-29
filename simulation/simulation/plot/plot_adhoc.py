14ee7bbd01e234b8d5e64ec6a9d59813010d4fac
Michele Mastropietro <michele.mastropietro@gmail.com>
2019-04-29 17:59:35 +0200

    try to plot adhoc title

--- simulation/simulation/plot/plot_adhoc.py
+++ simulation/simulation/plot/plot_adhoc.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 from simulation.parsers.parse_trace import parse_trace
+from simulation.util import get_sim_name
 
 def plot_adhoc(trace):
     plt.plot(trace.t, trace.ax_adhoc, '--',)
@@ -12,7 +13,6 @@ def plot_adhoc(trace):
     # plt.plot(trace.t, trace.a, 'r', linewidth=1)
     plt.grid()
     plt.legend();
-    plt.show()
 
 def parse_args(cli=None):
     parser = argparse.ArgumentParser()
@@ -27,7 +27,9 @@ def main(cli=None):
     trace = parse_trace(trace_path)
     print(trace.head())
     plot_adhoc(trace)
-
+    print("TRy to add title")
+    plt.gca().set_title(get_sim_name(args.sim_path))
+    plt.show()
 
 if __name__=='__main__':
     main()
