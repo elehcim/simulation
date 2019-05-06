Fix derotated arrays units, implement on orbit trasformation

simulation/derotate_simulation.py
@@ -32,6 +32,12 @@ def rotate_vec(vec, quat):
     return quaternion.as_float_array(new_vec)[:, 1:]
 
 
+def rotate_simarray(vec, quat):
+    """Rotate a SimArray array of 3-vectors `vec` given a quaternion `quat`"""
+    new_vec = rotate_vec(vec, quat)
+    return pynbody.array.SimArray(new_vec, vec.units)
+
+
 def get_all_keys(snap):
     """return all the (non derived) keys for all the families"""
     ak = set()
@@ -41,7 +47,7 @@ def get_all_keys(snap):
     ak.sort()
     return ak
 
-def rotate_snap(input_snap, quat, omega_mb, pivot, offset=None):
+def rotate_snap(input_snap, quat, omega_mb, pivot, offset=None, on_orbit_plane=False):
     f = input_snap
     s = pynbody.snapshot.new(dm=len(f.dm), gas=len(f.gas), star=len(f.star), order='gas,dm,star')
     # print(get_all_keys(f))
@@ -58,19 +64,27 @@ def rotate_snap(input_snap, quat, omega_mb, pivot, offset=None):
     for k in get_all_keys(f.s):
         s.s[k] = f.s[k]
 
-    new_pos, new_vel = derotate_pos_and_vel(s['pos'], s['vel'], quat, omega_mb, pivot)
+    new_pos, new_vel = derotate_pos_and_vel(f['pos'], f['vel'], quat, omega_mb, pivot)
+
+    if on_orbit_plane:
+        orbit_plane_quat = 1/np.sqrt(2) * np.quaternion(1, -1, 0, 0)
+        new_pos = rotate_simarray(new_pos, orbit_plane_quat)
+        new_vel = rotate_simarray(new_vel, orbit_plane_quat)
 
     del s['pos']
-    s['pos'] = pynbody.array.SimArray(new_pos.astype(f['pos'].dtype), f['pos'].units)
+    s['pos'] = new_pos.astype(f['pos'].dtype)
 
     del s['vel']
-    s['vel'] = pynbody.array.SimArray(new_vel.astype(f['vel'].dtype), f['vel'].units)
+    s['vel'] = new_vel.astype(f['vel'].dtype)
 
+    # print(f['pos'])
+    # print(s['pos'])
     # print(f.properties)
 
     s.properties = f.properties.copy()
     s.properties['z'] = f.properties['z']
     s.properties['boxsize'] = 60 * pynbody.units.kpc
+    # print(s.properties)
     return s
 
 
@@ -97,7 +111,7 @@ def get_quaternions(trace):
     return quaternion.as_quat_array(quat)
 
 
-def derotate_simulation(sim_path, new_path, snap_indexes=slice(None, None, None)):
+def derotate_simulation(sim_path, new_path, snap_indexes=slice(None, None, None), on_orbit_plane=False):
     sim = simulation.Simulation(sim_path, snap_indexes=snap_indexes)
 
     sim_name = get_sim_name(sim_path)
@@ -125,7 +139,7 @@ def derotate_simulation(sim_path, new_path, snap_indexes=slice(None, None, None)
     for i, (snap, q, om_mb) in enumerate(tqdm.tqdm(zip(sim, loc_quat, loc_omega_mb), total=len(sim))):
         # print(snap)
         # print(quat)
-        s_rot = rotate_snap(snap, q, om_mb, pivot, offset[i])
+        s_rot = rotate_snap(snap, q, om_mb, pivot, offset[i], on_orbit_plane)
         # This requires a change in source code of pynbody since it seems not possible
         # to set the dtype of the initial arrays as created by new()
         assert s_rot['mass'].dtype == np.float32
@@ -139,6 +153,9 @@ def parse_args(cli=None):
     parser = argparse.ArgumentParser()
     parser.add_argument(dest='sim_path', help="Path to the simulation snapshots")
     parser.add_argument("-o", '--outpath', help="Folder of the derotated snapshots", default=None)
+    parser.add_argument('--on-orbit-plane', help='Rotate snaps to be viewed from the orbit plane', action='store_true')
+    parser.add_argument('--start', help="First snap index", default=None, type=int)
+    parser.add_argument('--stop', help="Last snap index", default=None, type=int)
     parser.add_argument('-n', help="Take one every n snapshots", default=None, type=int)
     args = parser.parse_args(cli)
     return args
@@ -151,7 +168,7 @@ def main(cli=None):
         new_path = sim_name + "_derot"
     else:
         new_path = args.outpath
-    derotate_simulation(args.sim_path, new_path, slice(None, None, args.n))
+    derotate_simulation(args.sim_path, new_path, slice(args.start, args.stop, args.n))
 
 
 if __name__ == '__main__':