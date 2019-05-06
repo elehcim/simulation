1f8878229c3f043aa6f46a5af5c3ff04ed79d888
Michele Mastropietro <michele.mastropietro@gmail.com>
2019-05-06 15:26:10 +0200

    add on_orbit_plane and default to not saving single images of maps

--- simulation/simulation/save_maps.py
+++ simulation/simulation/save_maps.py
@@ -16,7 +16,7 @@ from astropy import units as u
 from simulation.luminosity import surface_brightness, kpc2pix, pix2kpc
 from simulation.util import setup_logger, get_sim_name, to_astropy_quantity, get_pivot, get_quat, get_omega_mb
 from simulation.angmom import faceon, sideon
-from simulation.derotate_simulation import derotate_pos_and_vel
+from simulation.derotate_simulation import derotate_pos_and_vel, rotate_on_orbit_plane
 
 R_EFF_BORDER = 10
 
@@ -58,7 +58,7 @@ class Snap:
     The main reason for this class is to have a uniform preprocessing.
     In particular the selection and the orientation should be well defined
     """
-    def __init__(self, snap_name, sphere_edge, derot_param=None):
+    def __init__(self, snap_name, sphere_edge, derot_param=None, on_orbit_plane=False):
         logger.info("Opening file {}".format(snap_name))
         s = pynbody.load(snap_name)
         self._snap = s
@@ -79,6 +79,11 @@ class Snap:
             logger.info("omega_mb: {}".format(omega_mb))
             logger.info("pivot:    {}".format(pivot))
             new_pos, new_vel = derotate_pos_and_vel(s['pos'], s['vel'], quat, omega_mb, pivot)
+
+            if on_orbit_plane:
+                logger.info("Rotating on the plane of the orbit...")
+                new_pos, new_vel = rotate_on_orbit_plane(new_pos, new_vel)
+
             s['pos'] = new_pos
             s['vel'] = new_vel
 
@@ -139,7 +144,9 @@ def get_outname(snap_name, out_dir, band, width, resolution, suffix=None, stem_o
     return out_name
 
 
-def single_snap_maps(snap_name, width, resolution, band='v', side=True, face=None, rotation=None, quat=None, omega_mb=None, pivot=None):
+def single_snap_maps(snap_name, width, resolution,
+                     band='v', side=True, face=None,
+                     quat=None, omega_mb=None, pivot=None, derotate=True, on_orbit_plane=False):
 
     """
     Parameters
@@ -150,22 +157,19 @@ def single_snap_maps(snap_name, width, resolution, band='v', side=True, face=Non
     pivot: np.array
 
     """
-    if quat is None or omega_mb is None or pivot is None:
+    if not derotate or quat is None or omega_mb is None or pivot is None:
         derot_param = None
     else:
         derot_param = {'quat': quat, 'omega_mb': omega_mb, 'pivot': pivot}
 
-    snap = Snap(os.path.expanduser(snap_name), sphere_edge=R_EFF_BORDER, derot_param=derot_param)
+    snap = Snap(os.path.expanduser(snap_name), sphere_edge=R_EFF_BORDER, derot_param=derot_param, on_orbit_plane=on_orbit_plane)
 
-    if rotation is not None:
-        rotation.apply(snap)
+    if side:
+        snap.sideon()
+    elif face:
+        snap.faceon()
     else:
-        if side:
-            snap.sideon()
-        elif face:
-            snap.faceon()
-        else:
-            logger.warning('No sideon or faceon indications: not rotating snap')
+        logger.warning('No sideon or faceon indications: not rotating snap')
 
     im = Imaging(snap.subsnap, width=width, resolution=resolution)
 
@@ -175,7 +179,10 @@ def single_snap_maps(snap_name, width, resolution, band='v', side=True, face=Non
 COLUMNS_UNITS = dict(vlos=u.km/u.s, sig=u.km/u.s, mag=u.mag * u.arcsec**-2, lum=u.solLum * u.pc**-2)
 
 
-def simulation_maps(sim_path, width, resolution, band='v', side=True, face=None, quat_dir=None, pivot=None):
+def simulation_maps(sim_path, width, resolution,
+                    band='v', side=True, face=None,
+                    quat_dir=None, pivot=None, derotate=True, on_orbit_plane=False,
+                    save_single_image=False):
 
     if not os.path.isdir(sim_path):
         raise RuntimeError('Simulation path should be a directory')
@@ -185,18 +192,21 @@ def simulation_maps(sim_path, width, resolution, band='v', side=True, face=None,
 
     sim_name = get_sim_name(sim_path)
 
-    quat_arr = get_quat(sim_name, quat_dir)
-    if quat_arr is None:
-        raise RuntimeError('Cannot find quaternion. First save it using save_quat.py')
+    if derotate:
+        quat_arr = get_quat(sim_name, quat_dir)
+        if quat_arr is None:
+            raise RuntimeError('Cannot find quaternion. First save it using save_quat.py')
 
-    assert len(quat_arr) == len(snap_list)
+        assert len(quat_arr) == len(snap_list)
 
-    if pivot is None:
-        pivot = get_pivot(sim_name)
+        if pivot is None:
+            pivot = get_pivot(sim_name)
 
-    omega_mb_arr = get_omega_mb(sim_name, quat_dir)
-    if omega_mb_arr is None:
-        raise RuntimeError('Cannot find omega_mb. First save it using save_quat.py')
+        omega_mb_arr = get_omega_mb(sim_name, quat_dir)
+        if omega_mb_arr is None:
+            raise RuntimeError('Cannot find omega_mb. First save it using save_quat.py')
+    else:
+        quat_arr = omega_mb_arr = pivot = None
 
     nan_arr = np.empty((resolution, resolution), dtype=np.float32)
     nan_arr[:] = np.nan
@@ -223,6 +233,7 @@ def simulation_maps(sim_path, width, resolution, band='v', side=True, face=None,
                                   quat=quat,
                                   omega_mb=omega_mb,
                                   pivot=pivot,
+                                  on_orbit_plane=on_orbit_plane,
                                   )
             # vlos = to_astropy_quantity(im.v_los_map())
             # sig = to_astropy_quantity(im.v_disp_map())
@@ -237,11 +248,13 @@ def simulation_maps(sim_path, width, resolution, band='v', side=True, face=None,
             maps_dict['sig'].append(sig)
             maps_dict['mag'].append(mag)
             maps_dict['lum'].append(lum)
-            mtbl_loc = Table([vlos, sig, mag, lum], names=['vlos','sig','mag', 'lum'], meta={'time':time, 'time_u': str(u.kpc*u.s*u.km**-1)})
-            for col_name, col in mtbl_loc.columns.items():
-                col.unit = COLUMNS_UNITS[col_name]
 
-            mtbl_loc.write(os.path.join(data_out_name, 'maps{}_img.fits.gz'.format(snap_name[-4:])), overwrite=True)
+            if save_single_image:
+                mtbl_loc = Table([vlos, sig, mag, lum], names=['vlos','sig','mag', 'lum'], meta={'time':time, 'time_u': str(u.kpc*u.s*u.km**-1)})
+                for col_name, col in mtbl_loc.columns.items():
+                    col.unit = COLUMNS_UNITS[col_name]
+
+                mtbl_loc.write(os.path.join(data_out_name, 'maps{}_img.fits.gz'.format(snap_name[-4:])), overwrite=True)
 
         except ValueError as e:
             # Usually is 'Insufficient particles around center to get velocity'
@@ -275,7 +288,10 @@ def parse_args(cli=None):
     parser.add_argument("--quat-dir", default='~/sim/analysis/ng_ana/data/quat', help='Directory of precomputed moving boxes quaternions')
     parser.add_argument('--quat', help='Quaternion value (space separated, e.g. "1 0 0 1.2")', type=str, default=None)
     parser.add_argument('--pivot', help='Coordinates of the pivot point (space separated, e.g. "30 30 30")', type=str, default=None)
+    parser.add_argument('-n', "--no-derot", action='store_false', help='Do not derotate')
+    parser.add_argument('--on-orbit-plane', action='store_true', help='Put snapshot on orbit plane')
     parser.add_argument("--out-dir", default=None)
+    parser.add_argument("--save-single-image", help="Save also single images in dedicated fits files", action='store_true')
     angmom_group.add_argument('--side', action='store_true')
     angmom_group.add_argument('--face', action='store_true')
 
@@ -298,6 +314,7 @@ def main(cli=None):
                               face=args.face,
                               quat=np.array(args.quat.split(), dtype=np.float64),
                               pivot=np.array(args.pivot.split(), dtype=np.float64),
+                              derotate=args.no_derot,
                               )
     elif args.sim_path is not None:
         simulation_maps(sim_path=args.sim_path,
@@ -308,6 +325,9 @@ def main(cli=None):
                         face=args.face,
                         quat_dir=args.quat_dir,
                         pivot=args.pivot,
+                        derotate=args.no_derot,
+                        on_orbit_plane=args.on_orbit_plane,
+                        save_single_image=args.save_single_image,
                         )
 
     return im
