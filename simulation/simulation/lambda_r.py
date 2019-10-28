3ab34e85dc79cd30b492821f42c9847cf5908270
Michele Mastropietro <michele.mastropietro@gmail.com>
2019-10-28 15:00:19 +0100

    simplify and fix usage of r_lim in lambda_r_profile

--- simulation/simulation/lambda_r.py
+++ simulation/simulation/lambda_r.py
@@ -11,8 +11,7 @@ def lambda_r(flux, v_los, sig_los):
     resolution = flux.shape[-1]
     x = y = np.arange(-resolution/2, resolution/2)
     xx, yy = np.meshgrid(x, y)
-    z = np.sqrt(xx**2 + yy**2)
-    r = z
+    r = np.sqrt(xx**2 + yy**2)
     return np.sum(flux * r * np.abs(v_los)) / np.sum(flux * r * np.sqrt(v_los**2 + sig_los**2))
 
 
@@ -23,8 +22,7 @@ def lambda_r_r_lim(flux, v_los, sig_los, width, r_lim):
     # print(f"flux.shape={flux.shape}")
     x = y = np.arange(-resolution/2, resolution/2)
     xx, yy = np.meshgrid(x, y)
-    z = np.sqrt(xx**2 + yy**2)
-    r = z
+    r = np.sqrt(xx**2 + yy**2)
     r_max = r_lim/width * resolution
     r[r>r_max] = 0
     return np.sum(flux * r * np.abs(v_los)) / np.sum(flux * r * np.sqrt(v_los**2 + sig_los**2))
@@ -41,12 +39,12 @@ def lambda_r_sim(flux, v_los, sig_los):
     print(f"r.shape={r.shape}")
     return np.sum(flux * r * np.abs(v_los), axis=(1,2)) / np.sum(flux * r * np.sqrt(v_los**2 + sig_los**2), axis=(1,2))
 
-def lambda_r_profile(flux, v_los, sig_los, width, bins=50):
+def lambda_r_profile(flux, v_los, sig_los, width, r_lim, bins=50):
     resolution = flux.shape[-1]
     if bins>resolution/2:
         warnings.warn(f'bins > resolution/2 ({bins} > {resolution//2}): this does not make a lot of sense...')
     prof = list()
-    r_arr = np.linspace(0, width/2, bins)
+    r_arr = np.linspace(0, r_lim, bins)
     for r in r_arr:
         prof.append(lambda_r_r_lim(flux, v_los, sig_los, width=width, r_lim=r))
     return np.array(prof)
