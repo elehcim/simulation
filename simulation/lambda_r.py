77dec4e75000d106b83081d1ebae4b2ec13c85c8
Michele Mastropietro <michele.mastropietro@gmail.com>
2019-10-29 15:31:38 +0100

    remove debug printing

--- simulation/lambda_r.py
+++ simulation/lambda_r.py
@@ -31,12 +31,12 @@ def lambda_r_r_lim(flux, v_los, sig_los, width, r_lim):
 def lambda_r_sim(flux, v_los, sig_los):
     assert flux.ndim == v_los.ndim == sig_los.ndim == 3
     resolution = flux.shape[-1]
-    print(f"flux.shape={flux.shape}")
+    # print(f"flux.shape={flux.shape}")
     x = y = np.arange(-resolution/2, resolution/2)
     xx, yy = np.meshgrid(x, y)
     z = np.sqrt(xx**2 + yy**2)
     r = z[np.newaxis]
-    print(f"r.shape={r.shape}")
+    # print(f"r.shape={r.shape}")
     return np.sum(flux * r * np.abs(v_los), axis=(1,2)) / np.sum(flux * r * np.sqrt(v_los**2 + sig_los**2), axis=(1,2))
 
 def lambda_r_profile(flux, v_los, sig_los, width, r_lim, bins=50):
