0ad1dd8602c2f988e3f83e60753f02b600c34688
Michele Mastropietro <michele.mastropietro@gmail.com>
2019-11-07 11:48:40 +0100

    aestetics

--- simulation/simulation/magnitude_transformation.py
+++ simulation/simulation/magnitude_transformation.py
@@ -6,24 +6,6 @@ import numpy as np
 # https://www.aanda.org/articles/aa/pdf/2006/46/aa6082-06.pdf
 
 
-def color_sdss_g_r(VmR):
-    """
-    Get SDSS (g'-r') color from Johnson filters color (V-R)
-    Jordi2006 eq. 7
-    """
-    return 1.646 * VmR - 0.139
-
-def rmR(VmR):
-    """
-    Get (r'-R) color from Johnson filters color (V-R)
-    Jordi2006 eq. 4
-    """
-    if VmR <= 0.93:
-        return 0.267 * VmR + 0.088
-    else:
-        return 0.77 * VmR - 0.37
-
-
 def get_sdss_u(U, B, V):
     g = get_sdss_g(B, V)
     u = g + 0.750 * (U-B) + 0.77*(B-V) + 0.72
@@ -49,6 +31,8 @@ def get_sdss_z(R, I):
     z = R - 1.584 * (R-I) + 0.386
     return z
 
+
+
 def color_sdss_r_i(RmI):
     """
     Get SDSS (r'-i') color from Johnson filters color (R-I)
@@ -56,9 +40,25 @@ def color_sdss_r_i(RmI):
     """
     return 1.007 * RmI - 0.236
 
-
 def color_sdss_g_i(V, R, I):
     """
     Get SDSS (g'-i') color from Johnson filters V, R, I
     """
     return color_sdss_g_r(V-R) - color_sdss_r_i(R - I)
+
+def color_sdss_g_r(VmR):
+    """
+    Get SDSS (g'-r') color from Johnson filters color (V-R)
+    Jordi2006 eq. 7
+    """
+    return 1.646 * VmR - 0.139
+
+def rmR(VmR):
+    """
+    Get (r'-R) color from Johnson filters color (V-R)
+    Jordi2006 eq. 4
+    """
+    if VmR <= 0.93:
+        return 0.267 * VmR + 0.088
+    else:
+        return 0.77 * VmR - 0.37
