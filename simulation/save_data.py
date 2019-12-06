39ce300769ada6331e8214ded35474e92b1d632f
Michele Mastropietro <michele.mastropietro@gmail.com>
2019-12-06 17:33:29 +0100

    fix bug and introduce 41p50 and the special ones

--- simulation/save_data.py
+++ simulation/save_data.py
@@ -3,14 +3,15 @@ from simulation.simdata import SIM_NAME_DICT, save_tables
 
 def parse_args(cli=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--on-orbit-plane', action='store_true', help='Put snapshot on orbit plane')
+    parser.add_argument('--no-on-orbit-plane', action='store_false', help='Put snapshot on orbit plane')
     args = parser.parse_args(cli)
     return args
 
 
 def main(cli=None):
     args = parse_args(cli)
-	save_tables(SIM_NAME_DICT.values(), orbit_sideon=args.om_orbit_plane)
+    orbit_sideon = args.no_on_orbit_plane
+    save_tables(SIM_NAME_DICT.values(), orbit_sideon=orbit_sideon)
 
 if __name__ == '__main__':
-	main()
+    main()
