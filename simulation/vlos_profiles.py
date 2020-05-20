c7d0d2cc1fc0c9209e1d293a811ca4ea55b2ee68
Michele Mastropietro <michele.mastropietro@gmail.com>
2020-05-20 20:24:21 +0200

    move pynbody related things out

--- simulation/vlos_profiles.py
+++ simulation/vlos_profiles.py
@@ -1,55 +1,10 @@
 # compute maximum LOS velocity from velocity map on the plane of the orbit
-import os
 import matplotlib
-import simulation
 import matplotlib.pyplot as plt
-import pynbody
 import numpy as np
 
-from astropy.table import Table
 from skimage.measure import profile_line
-from pynbody import units
-from pynbody.filt import Filter
-
-data_path = '/home/michele/sim/analysis/ng_ana/data/'
-
-
-class Slit(Filter):
-    """Emulate a Slit, so taking a slice at z=const of a snap"""
-    def __init__(self, x1, y1=None, x2=None, y2=None):
-        super().__init__()
-        self._descriptor = "slit"
-        x1, y1, x2, y2 = [units.Unit(x) if isinstance(x, str) else x for x in (x1, y1, x2, y2)]
-        if y1 is None:
-            y1 = x1
-        if x2 is None:
-            x2 = -x1
-        if y2 is None:
-            y2 = -y1
-
-        self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2
-
-    def __call__(self, sim):
-        x1, y1, x2, y2 = [x.in_units(sim["pos"].units, **sim["pos"].conversion_context())
-                                  if units.is_unit_like(x) else x
-                                  for x in (self.x1, self.y1, self.x2, self.y2)]
-
-        return (sim["x"] > x1) * (sim["x"] < x2) * (sim["y"] > y1) * (sim["y"] < y2)
-
-    def __repr__(self):
-        x1, y1, x2, y2 = ["'%s'" % str(x)
-                                  if units.is_unit_like(x) else x
-                                  for x in (self.x1, self.y1, self.x2, self.y2)]
-        return "Slit(%s, %s, %s, %s)" % (x1, y1, x2, y2)
-
-
-def get_vlos_map(sim_name, orbit_sideon):
-    if orbit_sideon:
-        tbl = Table.read(os.path.join(data_path, 'maps_orbit_sideon_sig_los', sim_name+'_maps.fits'))
-    else:
-        tbl = Table.read(os.path.join(data_path, 'maps_orbit_faceon_sig_los', sim_name+'_maps.fits'))
-    vlos_map = tbl['vlos']
-    return vlos_map
+from simulation.simdata import get_vlos_map
 
 
 def plot_maps(vlos_map, idx):
